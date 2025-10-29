c44ec46b431f29c4a8dd95f1d02596c3e572c47e
zhuoyu <zhuoyu.yong.2024@computing.smu.edu.sg>
2025-10-29 02:05:34 +0000

    change to ex1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -1,12 +1,14 @@
 <script>
     //import component
-    
+    import SoundIcon from './subcomponents/SoundIcon.vue';
+
     export default { 
         data() {
             return {
                 soundLevel: 0
             }
-        }
+        },
+        components: { SoundIcon },                            // <- register here
     }
 </script>
 
@@ -15,7 +17,8 @@
    Sound level
     <input type="number" min="0" max="3" v-model="soundLevel"> 
     <!-- TODO: Add Code Here to make use of the component -->
-    
+    <!-- Pass soundLevel to the child as a prop -->
+    <sound-icon :level="soundLevel" ></sound-icon>
 
 </template>
 
--- src/components/subcomponents/SoundIcon.vue
+++ src/components/subcomponents/SoundIcon.vue
@@ -6,10 +6,13 @@
             }
         },
         // TODO: add code to define a necessary prop
+        props: {
+            level: Number
+        }
     }
 </script>
 
 <template>
     <!-- TODO: add your template code here. Use span element. --> 
-    
+      <span>{{ soundEmojis[level] }}</span>
 </template>
